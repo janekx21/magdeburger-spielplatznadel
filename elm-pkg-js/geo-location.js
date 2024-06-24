9c745b43021aacc2e8eaa9dda9d5ce1a7b11c46a
Janek Winkler <janekx21@gmail.com>
2024-06-25 00:28:53 +0200

    wip geo location

--- elm-pkg-js/geo-location.js
+++ elm-pkg-js/geo-location.js
@@ -1,19 +1,22 @@
 exports.init = async function (app) {
-    // console.log("geo-location.js")
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition((_) => {});
-        navigator.geolocation.watchPosition((position) => {
-            const lat = position.coords.latitude;
-            const lng = position.coords.longitude;
-            let heading = position.coords.heading;
-            const location = {lat, lng};
-            const geoLocation = {location, heading}
-            // console.log("locationUpdated", geoLocation)
-            app.ports.geoLocationUpdated.send(JSON.stringify(geoLocation));
-        },
-        (_)=> {
-            // TODO some error handling like a user message
-            app.ports.geoLocationError.send("");
-        }) // TODO options
-    }
+  // console.log("geo-location.js")
+  if (navigator.geolocation) {
+    // The next request is only needed for the location promt
+    navigator.geolocation.getCurrentPosition((_) => {});
+    navigator.geolocation.watchPosition(
+      (position) => {
+        const lat = position.coords.latitude;
+        const lng = position.coords.longitude;
+        let heading = position.coords.heading;
+        const location = { lat, lng };
+        const geoLocation = { location, heading };
+        // console.log("locationUpdated", geoLocation)
+        app.ports.geoLocationUpdated.send(JSON.stringify(geoLocation));
+      },
+      (_) => {
+        // TODO some error handling like a user message
+        app.ports.geoLocationError.send("");
+      },
+    ); // TODO options
+  }
 };
