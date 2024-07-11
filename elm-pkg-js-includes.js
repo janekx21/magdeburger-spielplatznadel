0e1ceafee2c0c4161ab0d3e45a2757e0a2a9d972
Janek Winkler <janekx21@gmail.com>
2024-07-11 13:48:20 +0200

    fix include

--- elm-pkg-js-includes.js
+++ elm-pkg-js-includes.js
@@ -3,10 +3,12 @@
 // In future this will be more structured + restricted once the design is done
 
 const geo_location = require("./elm-pkg-js/geo-location");
+const storage = require("./elm-pkg-js/storage");
 
 exports.init = async function init(app) {
   // @WARNING: this only runs for Lamdera production deploys!
   // This file will not run in Local development, an equivalent to this is
   // automatically generated in Local Development for every file in elm-pkg-js/
   geo_location.init(app);
+  storage.init(app);
 };
