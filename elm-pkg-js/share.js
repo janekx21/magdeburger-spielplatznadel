d21bf6fd8acb8e95c690f69149d58e42aa705e4f
Janek Winkler <janekx21@gmail.com>
2024-07-15 12:48:19 +0200

    fixed share

--- elm-pkg-js/share.js
+++ elm-pkg-js/share.js
@@ -1,8 +1,6 @@
 exports.init = async function (app) {
   app.ports.share.subscribe((data) => {
-    console.log("sharing data", data);
     Promise.all(data.files.map(dataUrlToFile)).then((files) => {
-      console.log(files);
       if (navigator.share) {
         navigator
           .share({ ...data, files })
@@ -18,8 +16,6 @@ exports.init = async function (app) {
 // @param {string} dataUrl
 async function dataUrlToFile(dataUrl) {
   const response = await fetch(dataUrl);
-  console.log(response);
-  const blob = response.blob();
-  console.log(blob.type);
-  return new File([blob], "shared.png", { type: "image/png" });
+  const blob = await response.blob();
+  return new File([blob], "shared.png", { type: blob.type });
 }
